b38165a21e7e50bd23f367c8ee9cc009642dfb8b
lytuong222 <154073201+lytuong222@users.noreply.github.com>
2023-12-19 10:03:14 +0700

    Add files via upload

---
+++ dangky.php
@@ -0,0 +1,23 @@
+<h3>Đăng ký</h3>
+<form action="index.php?do=dangky_xuly" method="post">
+	<table class="Form">
+		<tr>
+			<td>Họ và tên:</td>
+			<td><input type="text" name="HoVaTen" /></td>
+		</tr>
+		<tr>
+			<td>Tên đăng nhập:</td>
+			<td><input type="text" name="TenDangNhap" /></td>
+		</tr>
+		<tr>
+			<td>Mật khẩu:</td>
+			<td><input type="password" name="MatKhau" /></td>
+		</tr>
+		<tr>
+			<td>Xác nhận mật khẩu:</td>
+			<td><input type="password" name="XacNhanMatKhau" /></td>
+		</tr>
+	</table>
+	
+	<input type="submit" value="Đăng ký" />
+</form>
---
+++ dangnhap.php
@@ -0,0 +1,15 @@
+<h3>Đăng nhập</h3>
+<form action="index.php?do=dangnhap_xuly" method="post">
+	<table class="Form">
+		<tr>
+			<td>Tên đăng nhập:</td>
+			<td><input type="text" name="TenDangNhap" /></td>
+		</tr>
+		<tr>
+			<td>Mật khẩu:</td>
+			<td><input type="password" name="MatKhau" /></td>
+		</tr>
+	</table>
+	
+	<input type="submit" value="Đăng nhập" />
+</form>
